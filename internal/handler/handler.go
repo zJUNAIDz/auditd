d612c5a86258dbef98fa600564cac1158d3e969e
Junaid Shaikh <junaidshaikh3967@gmail.com>
2026-06-19 03:16:21 +0530

    refactor Handler to include EventQueue in constructor for improved event management

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,13 +1,18 @@
 package handler
 
-import "github.com/zjunaidz/auditd/internal/service"
+import (
+	"github.com/zjunaidz/auditd/internal/queue"
+	"github.com/zjunaidz/auditd/internal/service"
+)
 
 type Handler struct {
 	svc *service.AuditService
+	queue *queue.EventQueue
 }
 
-func New(svc *service.AuditService) *Handler {
+func New(svc *service.AuditService, queue *queue.EventQueue) *Handler {
 	return &Handler{
 		svc: svc,
+		queue: queue,
 	}
 }
